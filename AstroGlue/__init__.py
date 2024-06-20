56963285aa0bda31a355cc4642240a0a12092d7b
S Kamalesh <105807625+Kam-s18@users.noreply.github.com>
2024-06-20 11:39:26 +0200

    Update __init__.py

--- AstroGlue/__init__.py
+++ AstroGlue/__init__.py
@@ -1,6 +1,11 @@
 # The version file is generated automatically by setuptools_scm
 from AstroGlue._version import version as __version__
 from .AstroGlue import AstroGlue
+import os
 
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using :0.0')
+    os.environ.__setitem__('DISPLAY', ':0.0')
+  
 c = AstroGlue()
 c.run()
